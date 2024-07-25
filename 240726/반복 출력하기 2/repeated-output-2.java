f76ee785dfae4f5cfede6a7f106935f4b8cb06e6
Chanseok <ckstjr93@naver.com>
2024-07-26 02:06:55 +0900

    Title: 반복 출력하기 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    public static void printHelloWorld(int n){
+        if(n == 0) return;
+        printHelloWorld(n-1);
+        System.out.println("HelloWorld");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        printHelloWorld(N);
+    }
+}
