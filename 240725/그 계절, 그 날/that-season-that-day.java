7d1cf1e10a2fb4af5e8f86982898882f4c082c6b
Chanseok <ckstjr93@naver.com>
2024-07-25 22:40:30 +0900

    Title: 그 계절, 그 날, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240725/그 계절, 그 날/that-season-that-day.java
+++ 240725/그 계절, 그 날/that-season-that-day.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Main {
 
-    public static boolean isF(int M){
-        if(M % 4 == 0){
-            if(M % 100 == 0){
-                if(M % 400 == 0) return true;
+    public static boolean isF(int Y){
+        if(Y % 4 == 0){
+            if(Y % 100 == 0){
+                if(Y % 400 == 0) return true;
                 return false;
             }
             return true;
@@ -19,11 +19,11 @@ public class Main {
         int[] day = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         int d = day[M];
-        if(M == 2 && isF(M)){            
+        if(M == 2 && isF(Y)){            
             d = 29;
         }
 
-        if(d >= D) return "-1";
+        if(d > D) return "-1";
 
         if(M >= 3 && M <= 5) return "Spring";
         else if(M >= 3 && M <= 5) return "Summer";
