48927208ea4d89cf5417347aa46e1d32f0d2b5b6
Chanseok <ckstjr93@naver.com>
2024-07-26 22:12:04 +0900

    Title: Time to Time, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/Time to Time/time-to-time.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int ans = (c * 60 + d) - (a * 60 + b);
+
+        System.out.println(ans);
+    }
+}
