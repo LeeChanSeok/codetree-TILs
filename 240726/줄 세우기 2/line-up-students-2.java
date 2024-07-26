df593cd2e982ba70dc1c1d9cced6d9071dc8383e
Chanseok <ckstjr93@naver.com>
2024-07-26 20:58:29 +0900

    Title: 줄 세우기 2, Time: 298ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240726/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+// 학생들의 정보를 나타내는 클래스 선언
+class Student {
+    int height;
+    int weight;
+    int id;
+    
+    public Student(int height, int weight, int id){
+        this.height = height;
+        this.weight = weight;
+        this.id = id;
+    }
+};
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+        for(int i = 0; i < n; i++) {
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+
+            // Student 객체를 생성해 리스트에 추가합니다.
+            students[i] = new Student(height, weight, i + 1);
+        }
+
+        // custom comparator를 활용한 정렬
+        Arrays.sort(students, new Comparator<Student>() {  
+            @Override
+            public int compare(Student a, Student b) { // 키를 기준 오름차순 정렬합니다.
+                if(a.height == b.height) return b.weight - a.weight;
+                return a.height - b.height;
+                
+            }
+        });
+
+        for (int i = 0; i < n; i++){
+            System.out.print(students[i].height + " ");
+            System.out.print(students[i].weight + " ");
+            System.out.println(students[i].id);
+        }
+
+    }
+}
