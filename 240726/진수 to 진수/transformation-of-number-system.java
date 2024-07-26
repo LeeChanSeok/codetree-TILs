8fc24566a22e00fddaf24aef1133ab63a9d37196
Chanseok <ckstjr93@naver.com>
2024-07-26 22:48:43 +0900

    Title: 진수 to 진수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240726/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    static int a, b;
+
+    public static void binary(int n){
+        if(n == 1) {
+            System.out.print("1");
+            return;
+        }
+
+        binary(n/b);
+        System.out.print(n%b);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        char[] arr = sc.next().toCharArray();
+        int num = 0;
+
+        for(int i = 0; i < arr.length; i++)
+            num = num * a + (arr[i] - '0');
+        
+        if(num == 0) System.out.println(0);
+        else binary(num);
+    }
+}
