f4f64706744c9896869a7ed5234bb926ef0fb052
Chanseok <ckstjr93@naver.com>
2024-07-26 22:16:46 +0900

    Title: Date to Date, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/Date to Date/date-to-date.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+            
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        //                                1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int elapsedDays = -d1 + 1;
+        for(int i = m1; i < m2; ++i){
+            elapsedDays += num_of_days[i];
+        }
+        elapsedDays += d2;
+
+        System.out.print(elapsedDays);
+    }
+}
