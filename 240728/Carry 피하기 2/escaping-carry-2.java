765c1f78ad378895d19602fce74d9c2d6fd677d3
Chanseok <ckstjr93@naver.com>
2024-07-28 04:41:56 +0900

    Title: Carry 피하기 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240728/Carry 피하기 2/escaping-carry-2.java
+++ 240728/Carry 피하기 2/escaping-carry-2.java
@@ -15,7 +15,7 @@ public class Main {
 			arr[i] = sc.nextInt();
 		}
 
-		int ans = 0;
+		int ans = -1;
 		for (int i = 0; i < n - 2; ++i) {
 			for (int j = i + 1; j < n - 1; ++j) {
 				for (int k = j + 1; k < n; ++k) {
@@ -40,7 +40,7 @@ public class Main {
 		int sum = 0;
 		while(a != 0 || b != 0 || c != 0) {
 			int digit = a % 10 + b % 10 + c % 10;
-			if(digit >= 10) return 0;
+			if(digit >= 10) return -1;
 			sum = sum + (int)Math.pow(10, p) * digit; 
 			
 			a /= 10;
