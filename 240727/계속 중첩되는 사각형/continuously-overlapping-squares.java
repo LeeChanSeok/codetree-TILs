7ef34c741a31edc317102e00d46085222db78cfd
Chanseok <ckstjr93@naver.com>
2024-07-27 19:21:16 +0900

    Title: 계속 중첩되는 사각형, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240727/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {    
+    public static final int OFFSET = 100;
+    public static final int MAX_N = 200;   
+    
+    public static int n;
+    public static int[][] board = new int[MAX_N + 1][MAX_N + 1];   
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for(int k = 0; k < n; k++) {
+            int x1 = sc.nextInt() + OFFSET;
+            int y1 = sc.nextInt() + OFFSET;
+            int x2 = sc.nextInt() + OFFSET;
+            int y2 = sc.nextInt() + OFFSET;
+
+            
+            for(int i = x1; i < x2; ++i){
+                for(int j = y1; j < y2; ++j){
+                    board[i][j] = (k % 2 + 1);
+                }
+            }
+
+        }
+        
+        int area = 0;        
+        for(int i = 0; i <= MAX_N; i++){
+            for(int j = 0; j <= MAX_N; j++){
+                if(board[i][j] == 2) {
+                    ++area;
+                }
+            }
+
+        }
+
+        System.out.print(area);
+    }
+
+}
