424aa15971696490f9ee212dc362a81aff162669
Chanseok <ckstjr93@naver.com>
2024-07-31 01:10:14 +0900

    Title: 상해버린 치즈, Time: 407ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240731/상해버린 치즈/rotten-cheese.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int D = sc.nextInt();
+		int S = sc.nextInt();
+
+		int[][] A = new int[D][3];
+
+		for (int i = 0; i < D; i++) {
+			int p = sc.nextInt();
+			int m = sc.nextInt();
+			int t = sc.nextInt();
+
+			A[i][0] = p;
+			A[i][1] = m;
+			A[i][2] = t;
+		}
+
+		Arrays.sort(A, (a1, a2) -> a1[2] - a2[2]);
+
+		int[] B = new int[N+1];
+		for (int i = 0; i < S; i++) {
+			int p = sc.nextInt();
+			int t = sc.nextInt();
+
+			B[p] = t;
+		}
+
+		int ans = 0;
+		for (int i = 1; i <= M; ++i) {
+
+			int cnt = 0;
+			boolean[] isEat = new boolean[N+1];
+			boolean[] isSick = new boolean[N+1];
+			boolean isPro = true;
+			
+			for (int j = 1; j <= N; ++j) {
+				
+				int t = B[j];				
+			
+				for (int k = 0; k < D; ++k) {
+					if(A[k][1] == i && A[k][0] == j) {
+						isEat[j] = true;
+						
+						if(A[k][2] < t) {
+							isSick[j] = true;
+						}else {
+							if(t == 0) continue;
+							isPro = false;
+						}
+					}
+					
+					
+					
+				}
+				
+				if((isEat[j] ) ) {
+					++cnt;
+				}else if(!isEat[j] && t != 0){
+					isPro = false;
+					break;
+				}
+			}
+			ans = Math.max(ans, cnt);
+		}
+
+		System.out.println(ans);
+	}
+}
