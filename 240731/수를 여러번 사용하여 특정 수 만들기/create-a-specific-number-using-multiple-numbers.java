a94469e1c2f5b8f59d84ade35ae3252b60c88df8
Chanseok <ckstjr93@naver.com>
2024-07-31 06:02:53 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240731/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int A = sc.nextInt();
+    	int B = sc.nextInt();
+    	int C = sc.nextInt();
+    	
+    	int ans = 0;
+    	int i = 0, j = 0;
+    	while(A * i < C) {
+    		int score = A * i;
+    		j = 0;
+    		while(score < C) {
+    			ans = Math.max(ans, score);
+    			score += B * (++j);
+    		}
+    		++i;
+    	}
+        
+        System.out.println(ans);
+    }
+}
