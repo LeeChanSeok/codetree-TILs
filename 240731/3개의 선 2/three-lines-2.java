7d164c86156e0d8579e90795c20fd5e5fce02d82
Chanseok <ckstjr93@naver.com>
2024-07-31 06:20:52 +0900

    Title: 3개의 선 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240731/3개의 선 2/three-lines-2.java
@@ -0,0 +1,96 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	static final int MAX = 10;
+
+	static int N;
+	static int[][] point;
+	static boolean[] X;
+	static boolean[] Y;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		point = new int[N][2];
+		X = new boolean[MAX + 1];
+		Y = new boolean[MAX + 1];
+
+		for (int i = 0; i < N; ++i) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+
+			X[x] = true;
+			Y[y] = true;
+		}
+
+		int ans = 0;
+		if (solution())
+			ans = 1;
+
+		System.out.println(ans);
+	}
+
+	private static boolean solution() {
+		
+		int xCnt = 0, yCnt = 0;
+		for (int i = 0; i < N; ++i) {
+			if (X[i])
+				xCnt++;
+			if (Y[i])
+				yCnt++;
+		}
+
+		if (xCnt == 3 || yCnt == 3)
+			return true;
+
+		// x 2개, y 1개
+		if (solution2())
+			return true;
+		// x 1개, y 2개
+		if (solution3())
+			return true;
+
+		return false;
+	}
+
+	private static boolean solution3() {
+		for (int y1 = 0; y1 <= MAX - 2; ++y1) {
+			for (int y2 = y1 + 1; y2 <= MAX; ++y2) {
+				for (int x1 = 0; x1 <= MAX; ++x1) {
+
+					for (int i = 0; i < N; ++i) {
+						int x = point[i][0];
+						int y = point[i][1];
+						if (x != x1 || y != y1 || y != y2)
+							return false;
+					}
+
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private static boolean solution2() {
+		for (int x1 = 0; x1 <= MAX - 2; ++x1) {
+			for (int x2 = x1 + 1; x2 <= MAX; ++x2) {
+				for (int y1 = 0; y1 <= MAX; ++y1) {
+
+					for (int i = 0; i < N; ++i) {
+						int x = point[i][0];
+						int y = point[i][1];
+						if (x != x1 || x != x2 || y != y1)
+							return false;
+					}
+
+				}
+			}
+		}
+
+		return true;
+	}
+}
