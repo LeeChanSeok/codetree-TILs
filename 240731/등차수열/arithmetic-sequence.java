89f261a10b27f050f8ed0b9de9877ecda88889af
Chanseok <ckstjr93@naver.com>
2024-07-31 03:06:34 +0900

    Title: 등차수열, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240731/등차수열/arithmetic-sequence.java
+++ 240731/등차수열/arithmetic-sequence.java
@@ -15,16 +15,14 @@ public class Main {
     	}
         
     	int ans = 0;
-        for(int i = 0; i < n -1; ++i) {
-        	
-        	for(int j = i + 1; j < n; ++j) {
-        		
-        		for(int k = -100; k <= 100; ++k) {
-        			if(arr[j] - k == k - arr[i]) ++ans;
-        		}
-        		
-        	}
-        	
+    	for(int k = -100; k <= 100; ++k) {
+    		int cnt = 0;
+    		for(int i = 0; i < n -1; ++i) {
+    			for(int j = i + 1; j < n; ++j) {
+        			if(arr[j] - k == k - arr[i]) ++cnt;
+    			}	
+    			ans = Math.max(ans,  cnt);
+			}
         }
         
         System.out.println(ans);
