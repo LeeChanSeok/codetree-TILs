f0969299faa7eda58847af8c4f68729ae282793b
Chanseok <ckstjr93@naver.com>
2024-08-01 22:12:18 +0900

    Title: 달리자, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240801/달리자/run-and-run.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int MAX = 100;
+	static int N;
+	static int[] X;
+	static int[] Y;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		X = new int[N + 1];
+		Y = new int[N + 1];
+
+		for (int i = 0; i < N; ++i) {
+			X[i] = sc.nextInt();
+			Y[i] = sc.nextInt();
+		}
+
+		int ans = 0;
+		for (int i = 0; i < N; ++i) {
+			int diff = X[i] - Y[i];
+			ans += diff;
+			X[i + 1] += diff;
+
+		}
+		
+		System.out.println(ans);
+
+	}
+
+}
