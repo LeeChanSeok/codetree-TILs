d0436a0bbebeb462f15aa5bb815d6b2d65165dc6
Chanseok <ckstjr93@naver.com>
2024-08-02 01:44:18 +0900

    Title: X 달리기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240802/X 달리기/x-run.java
+++ 240802/X 달리기/x-run.java
@@ -3,73 +3,34 @@ import java.util.Scanner;
 
 public class Main {
 
-	static class Data implements Comparable<Data> {
-		int t, v, p;
-
-		public Data(int t, int v, int p) {
-			super();
-			this.t = t;
-			this.v = v;
-			this.p = p;
-		}
-
-		@Override
-		public int compareTo(Data o) {
-			if (this.t == o.t) {
-				if (o.p == this.p)
-					return o.v - this.v;
-				return o.p - this.p;
-			}
-			return this.t - o.t;
-		}
-		
-	}
-
 	static int X;
-	static PriorityQueue<Data> q;
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
 		X = sc.nextInt();
-
-		q = new PriorityQueue<>();
-
-		q.offer(new Data(1, 1, 1));
-		int ans = solution();
-		System.out.println(ans);
-	}
-
-	private static int solution() {
-
-		while (!q.isEmpty()) {
-			Data cur = q.poll();
-
-			int t = cur.t;
-			int v = cur.v;
-			int p = cur.p;
-
-			if (p > X)
-				continue;
-			if (p == X) {
-				if (v == 1)
-					return t;
-				continue;
-			}
-
-			int nv = v + 1;
-			q.offer(new Data(t + 1, nv, p + nv));
-
-			nv = v;
-			q.offer(new Data(t + 1, nv, p + nv));
-
-			nv = v - 1;
-			if (nv != 0)
-				q.offer(new Data(t + 1, nv, p + nv));
+		
+		int cur_pos = 0;
+		int cur_speed = 1;
+		int time = 0;
+		
+		while(true) {
+			cur_pos += cur_speed;
+			++cur_speed;
+			++time;
+			
+			if(cur_pos == X) break;
+			
+			int sum = cur_speed * (cur_speed + 1) / 2;
+			if(cur_pos + sum > X) --cur_speed;
+			
+			sum = cur_speed * (cur_speed + 1) / 2;
+			if(cur_pos + sum > X) --cur_speed;
+			
 		}
-
-		return -1;
+		
+		System.out.println(time);
 	}
+
 
 }
