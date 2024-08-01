fd5e5a004775280ff027350bb4179c3d5e55f5ff
Chanseok <ckstjr93@naver.com>
2024-08-02 00:00:48 +0900

    Title: 코딩톡, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240802/코딩톡/coding-talk.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int P = sc.nextInt();
+
+		char[] X = new char[N];
+		int[] Y = new int[N];
+
+		for (int i = 0; i < M; ++i) {
+			X[i] = sc.next().charAt(0);
+			Y[i] = sc.nextInt();
+		}
+
+		boolean[] read = new boolean[N + 1];
+		for (int i = 0; i < M; ++i) {
+			char c = X[i];
+			int y = Y[i];
+
+			if(i >= P - 1 || y == Y[P-1]) 
+				read[c - 'A'] = true;
+		}
+
+		if(Y[P-1] != 0) {
+			for (int i = 0; i < N; ++i) {
+				if (!read[i])
+					System.out.print((char) (i + 'A') + " ");
+			}
+		}
+
+	}
+
+}
