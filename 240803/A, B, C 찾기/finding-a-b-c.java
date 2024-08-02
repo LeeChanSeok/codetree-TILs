4462a98511706651ca595d54a1226135589f7afe
Chanseok <ckstjr93@naver.com>
2024-08-03 00:56:07 +0900

    Title: A, B, C 찾기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240803/A, B, C 찾기/finding-a-b-c.java
+++ 240803/A, B, C 찾기/finding-a-b-c.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int n = 6;
+		int n = 7;
 		int[] X = new int[n];
 		for (int i = 0; i < n; ++i) {
 			X[i] = sc.nextInt();			
