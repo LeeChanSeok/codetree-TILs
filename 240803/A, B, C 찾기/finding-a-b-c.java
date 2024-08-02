c73d8cc239c3036492e655ff7d6a5648d32cef22
Chanseok <ckstjr93@naver.com>
2024-08-03 00:54:23 +0900

    Title: A, B, C 찾기, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240803/A, B, C 찾기/finding-a-b-c.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = 6;
+		int[] X = new int[n];
+		for (int i = 0; i < n; ++i) {
+			X[i] = sc.nextInt();			
+		}
+
+		Arrays.sort(X);
+		
+		int A = X[0];
+		int B = X[1];
+		int C = X[n-1] - (A + B);
+		
+		System.out.print(A + " " + B + " " + C);
+	}
+}
