ff33de30d4aa7fffa6b9373e3656ce2c6ec2d505
Chanseok <ckstjr93@naver.com>
2024-08-03 23:27:07 +0900

    Title: 황금비율 토스트, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240803/황금비율 토스트/golden-toast.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        LinkedList<Character> list = new LinkedList<>();
+        String str = sc.next();
+        
+        for(int i = 0; i < N; ++i) {
+        	list.add(str.charAt(i));
+        }
+        
+        ListIterator<Character> iter = list.listIterator(N);
+        for(int i = 0; i < N; ++i) {
+        	char cmd = sc.next().charAt(0);
+        	
+        	if(cmd == 'L') {
+        		if(iter.hasPrevious()) iter.previous();
+        	} else if(cmd == 'R') {
+        		if(iter.hasNext()) iter.next();
+        	} else if(cmd == 'D') {
+        		if(iter.hasNext()) {
+        			iter.next();
+        			iter.remove();
+        		}
+        	}else if(cmd == 'P') {
+        		iter.add(sc.next().charAt(0));
+        	}
+        }
+        
+        iter = list.listIterator();
+        while(iter.hasNext()) {
+        	System.out.print(iter.next());
+        }
+    }
+}
