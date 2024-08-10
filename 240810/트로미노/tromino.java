2ac6896cbe890e02f09221b51a94771e0c6a5bce
Chanseok <ckstjr93@naver.com>
2024-08-10 21:20:06 +0900

    Title: 트로미노, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240810/트로미노/tromino.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		int ans = 0;
+		for(int i = 0; i < n; ++i) {
+			int sum = 0;
+			for(int j = 0; j < n - 2; ++j) {
+				for(int k = 0; k < 3; ++k) {
+					sum += arr[i][j+k];
+				}
+				ans = Math.max(ans, sum);
+			}
+		}
+		
+		for(int j = 0; j < n; ++j) {
+			int sum = 0;
+			for(int i = 0; i < n - 2; ++i) {
+				for(int k = 0; k < 3; ++k) {
+					sum += arr[i+k][j];
+				}
+				ans = Math.max(ans, sum);
+			}
+		}
+		
+		for(int i = 0; i < n - 1; ++i) {
+			int sum = 0;
+			for(int j = 0; j < n - 1; ++j) {
+				
+				sum = arr[i][j] + arr[i][j+1] + arr[i+1][j];
+				ans = Math.max(ans, sum);
+				
+				sum = arr[i+1][j+1] + arr[i][j+1] + arr[i+1][j];
+				ans = Math.max(ans, sum);
+				
+				sum = arr[i][j] + arr[i][j+1] + arr[i+1][j+1];
+				ans = Math.max(ans, sum);
+				
+				sum = arr[i][j] + arr[i+1][j+1] + arr[i+1][j];
+				ans = Math.max(ans, sum);
+				
+				
+			}
+		}
+		
+		System.out.println(ans);
+
+	}
+
+}
