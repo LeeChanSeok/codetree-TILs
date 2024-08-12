6b9317903c4e872e46c47aa02767d87bb70edd85
Chanseok <ckstjr93@naver.com>
2024-08-13 02:06:46 +0900

    Title: 1차원 젠가, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240813/1차원 젠가/jenga-1d.java
+++ 240813/1차원 젠가/jenga-1d.java
@@ -5,36 +5,37 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+
 		int[] arr = new int[n];
-		for(int i = 0; i < n; ++i) {
+		for (int i = 0; i < n; ++i) {
 			arr[i] = sc.nextInt();
 		}
-		
-		int endOfArray = n -1;
+
+		int endOfArray = n - 1;
 		int s = sc.nextInt() - 1;
 		int e = sc.nextInt() - 1;
-		
+
 		int len = e - s;
-		for(int i = s, j = i + len + 1; i <= e && j <= endOfArray; ++i, ++j) {
+		for (int i = s, j = i + len + 1; j <= endOfArray; ++i, ++j) {
 			arr[i] = arr[j];
 		}
-		
+
 		endOfArray -= (len + 1);
 		s = sc.nextInt() - 1;
 		e = sc.nextInt() - 1;
-		
+
 		len = e - s;
-		for(int i = s, j = i + len + 1; i <= e && j <= endOfArray; ++i, ++j) {
+		for (int i = s, j = i + len + 1; j <= endOfArray; ++i, ++j) {
 			arr[i] = arr[j];
 		}
-		
+
 		endOfArray -= (len + 1);
+
 		System.out.println(endOfArray + 1);
-		for(int i = 0; i <= endOfArray; ++i) {
+		for (int i = 0; i <= endOfArray; ++i) {
 			System.out.println(arr[i]);
 		}
-		
+
 	}
 
 }
