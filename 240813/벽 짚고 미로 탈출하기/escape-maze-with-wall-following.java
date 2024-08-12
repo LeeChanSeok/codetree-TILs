3cf575d2c9a409d2fa7d5a98bc2ff3a264c6e0ee
Chanseok <ckstjr93@naver.com>
2024-08-13 05:31:09 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 3030ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240813/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -0,0 +1,83 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { 1, 0, -1, 0 };
+
+	static int n, x, y;
+	static char[][] arr;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		x = sc.nextInt() - 1;
+		y = sc.nextInt() - 1;
+
+		arr = new char[n][n];
+		for (int i = 0; i < n; ++i) {
+			arr[i] = sc.next().toCharArray();
+		}
+		int ans = simulation();
+		System.out.println(ans);
+	}
+
+	private static int simulation() {
+
+		boolean[][][] visited = new boolean[n][n][4];
+		visited[x][y][0] = true;
+		
+		int move = 0;
+		int dir = 0;
+		while(true) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			
+			if(!inRange(nx, ny)) return move + 1;
+			if(visited[nx][ny][dir]) return -1;
+			if(arr[nx][ny] == '.') {
+				x = nx; y = ny;
+				dir = (dir + 1) % 4;
+				++move;
+			}
+			if(arr[nx][ny] == '#') {
+				dir = (dir + 3) % 4;
+			}
+		}
+		
+	}
+
+	private static boolean isExit() {
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {x, y});
+		
+		boolean[][] visited = new boolean[n][n];
+		visited[x][y] = true;
+		
+		boolean isExit = false;
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			
+			for(int d = 0; d < 4; ++d) {
+				int nx = x + dx[d];
+				int ny = y + dy[d];
+				
+				if(!inRange(nx, ny)) return true;
+				if(arr[nx][ny] == '#') continue;
+				if(visited[nx][ny]) continue;
+				
+				q.offer(new int[] {nx, ny});
+				visited[nx][ny] = true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean inRange(int x, int y) {
+		if(x < 0 || x >= n || y < 0 || y >= n) return false;
+		return true;
+	}
+
+}
