9c1f4f6e2306609bd319a9cea72effcd22079de5
Chanseok <ckstjr93@naver.com>
2024-08-20 02:42:17 +0900

    Title: 낮은 지점들, Time: 949ms, Memory: 38MB, Status: Wrong Answer - CodeTree

---
+++ 240820/낮은 지점들/lowest-points.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class Main {
+
+	static int n, m, k;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < n; ++i) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+
+			int tmp = map.getOrDefault(x, Integer.MAX_VALUE);
+			map.put(x, Math.min(tmp, y));
+		}
+
+		int sum = 0;
+		for (Entry<Integer, Integer> entry : map.entrySet()) {
+			sum += entry.getValue();
+
+		}
+		System.out.println(sum);
+
+	}
+
+}
