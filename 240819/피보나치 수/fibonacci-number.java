f838bd12d329797912f7acb435e29d7fd7c30e15
Chanseok <ckstjr93@naver.com>
2024-08-19 21:20:56 +0900

    Title: 피보나치 수, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240819/피보나치 수/fibonacci-number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int n, m, k;
+
+	public static void main(String[] args) {
+		int MAX = 45;
+
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+
+		int[] dp = new int[MAX + 1];
+		dp[1] = dp[2] = 1;
+
+		for (int i = 3; i <= n; ++i) {
+			dp[i] = dp[i - 1] * i;
+		}
+
+		System.out.println(dp[n]);
+
+	}
+
+}
