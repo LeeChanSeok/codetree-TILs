c295edeeabbfa507ec170b68517411376bcc22bb
Chanseok <ckstjr93@naver.com>
2024-08-20 01:39:41 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 339ms, Memory: 23MB, Status: Passed - CodeTree

--- 240820/정수 사각형 최댓값의 최소/minimax-path-in-square.java
+++ 240820/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -21,7 +21,7 @@ public class Main {
 		dp[0][0] = arr[0][0];
 		for (int i = 1; i < n; ++i) {
 			dp[0][i] = Math.max(dp[0][i - 1], arr[0][i]);
-			dp[i][0] = Math.max(dp[i - 1][0], arr[0][i]);
+			dp[i][0] = Math.max(dp[i - 1][0], arr[i][0]);
 		}
 
 		for (int i = 1; i < n; ++i) {
