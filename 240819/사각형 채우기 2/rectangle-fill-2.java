efd66375505b9cae0a0f0453193a3b09a4ed2ab3
Chanseok <ckstjr93@naver.com>
2024-08-19 21:31:08 +0900

    Title: 사각형 채우기 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240819/사각형 채우기 2/rectangle-fill-2.java
+++ 240819/사각형 채우기 2/rectangle-fill-2.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		int MAX = 1000;
-
+		int DIV = 10007;
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 
@@ -15,7 +15,7 @@ public class Main {
 		dp[2] = 3;
 
 		for (int i = 3; i <= n; ++i) {
-			dp[i] = dp[i-1] + dp[i-2] * 2;
+			dp[i] = (dp[i-1] + dp[i-2] * 2) % DIV;
 		}
 
 		System.out.println(dp[n]);
