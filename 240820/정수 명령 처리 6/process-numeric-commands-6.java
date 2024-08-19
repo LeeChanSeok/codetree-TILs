98aeb770e114c4669e78627b3680b67b008333d0
Chanseok <ckstjr93@naver.com>
2024-08-20 04:53:41 +0900

    Title: 정수 명령 처리 6, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240820/정수 명령 처리 6/process-numeric-commands-6.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+	static int n, m, k;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+		for (int i = 0; i < n; ++i) {
+			String cmd = sc.next();
+
+			if (cmd.equals("push")) {
+				int k = sc.nextInt();
+				pq.offer(k);
+			} else if (cmd.equals("pop")) {
+				System.out.println(pq.poll());
+			} else if (cmd.equals("size")) {
+				System.out.println(pq.size());
+			} else if (cmd.equals("empty")) {
+				if (pq.isEmpty())
+					System.out.println(1);
+				else
+					System.out.println(0);
+			} else if (cmd.equals("top")) {
+				System.out.println(pq.peek());
+			}
+		}
+
+	}
+
+}
