8fe398d7f42cfd20fb4c74301ab9fc07660d4100
Chanseok <ckstjr93@naver.com>
2024-08-19 21:25:51 +0900

    Title: 계단 오르기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240819/계단 오르기/climbing-stairs.java
+++ 240819/계단 오르기/climbing-stairs.java
@@ -6,15 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		int MAX = 1000;
+		int DIV = 10007;
 
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 
-		int[] dp = new int[MAX + 1];
+		long[] dp = new long[MAX + 1];
 		dp[2] = dp[3] = 1;
 
 		for (int i = 4; i <= n; ++i) {
-			dp[i] = dp[i - 2] + dp[i - 3];
+			dp[i] = (dp[i - 2] + dp[i - 3]) % DIV;
 		}
 
 		System.out.println(dp[n]);
