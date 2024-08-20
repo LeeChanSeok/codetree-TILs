ee01ec527d5198c84b6b8c2188ffe203c0778c70
Chanseok <ckstjr93@naver.com>
2024-08-20 13:16:31 +0900

    Title: 점 개수 세기 3, Time: 3031ms, Memory: 95MB, Status: Time Limit Exceed - CodeTree

---
+++ 240820/점 개수 세기 3/count-number-of-points-3.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+	static int n, m, k;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		m = sc.nextInt();
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; ++i) {
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < n; ++i) {
+			map.put(arr[i], i);
+		}
+
+		for (int i = 0; i < m; ++i) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			int s = map.get(a);
+			int e = map.get(b);
+			System.out.printf("%d\n", e - s + 1);
+		}
+
+	}
+
+}
