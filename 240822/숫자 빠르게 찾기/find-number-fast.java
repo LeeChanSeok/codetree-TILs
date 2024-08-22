bf10d03436ede10366bbdbf340d2f1035d5b9e66
Chanseok <ckstjr93@naver.com>
2024-08-22 19:35:05 +0900

    Title: 숫자 빠르게 찾기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240822/숫자 빠르게 찾기/find-number-fast.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int n, m, k;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		int[] A = new int[n];
+		for (int i = 0; i < n; ++i) {
+			A[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < m; ++i) {
+			System.out.println(binarySearch(A, sc.nextInt()));
+		}
+
+	}
+
+	private static int binarySearch(int[] arr, int target) {
+
+		int l = 0, r = arr.length;
+		while (l <= r) {
+			int mid = (l + r) / 2;
+
+			if (arr[mid] == target)
+				return mid + 1;
+
+			if (arr[mid] > target)
+				r = mid - 1;
+			else
+				l = mid + 1;
+		}
+
+		return -1;
+	}
+
+}
