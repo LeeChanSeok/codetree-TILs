25abac2630968a5d3771687cae353c563c3bbca5
Chanseok <ckstjr93@naver.com>
2024-08-25 05:07:20 +0900

    Title: 계단 오르기 2, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240825/계단 오르기 2/climbing-stairs-2.java
+++ 240825/계단 오르기 2/climbing-stairs-2.java
@@ -12,18 +12,18 @@ public class Main {
 			A[i] = sc.nextInt();
 		}
 
-		int[][] dp = new int[n + 1][3];
+		int[][] dp = new int[n + 1][4];
 
 		for (int i = 1; i <= n; ++i) {
 
 			// 1칸
-			for (int j = 1; j <= 2; ++j) {
+			for (int j = 1; j <= 3; ++j) {
 				dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + A[i]);
 			}
 
 			// 2칸
 			if (i >= 2) {
-				for (int j = 0; j <= 2; ++j) {
+				for (int j = 0; j <= 3; ++j) {
 					dp[i][j] = Math.max(dp[i][j], dp[i - 2][j] + A[i]);
 				}
 			}
@@ -31,7 +31,7 @@ public class Main {
 		}
 
 		int max = 0;
-		for (int j = 0; j <= 2; ++j) {
+		for (int j = 0; j <= 3; ++j) {
 			max = Math.max(max, dp[n][j]);
 		}
 		System.out.println(max);
