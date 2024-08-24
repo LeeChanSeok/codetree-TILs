75b1241aa0321edd38378d97da899cb3082a4104
Chanseok <ckstjr93@naver.com>
2024-08-25 05:18:33 +0900

    Title: 계단 오르기 2, Time: 206ms, Memory: 12MB, Status: Passed - CodeTree

--- 240825/계단 오르기 2/climbing-stairs-2.java
+++ 240825/계단 오르기 2/climbing-stairs-2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -13,17 +14,26 @@ public class Main {
 		}
 
 		int[][] dp = new int[n + 1][4];
+		for (int i = 0; i <= n; ++i) {
+			Arrays.fill(dp[i], -1);
+		}
+
+		dp[0][0] = 0;
 
 		for (int i = 1; i <= n; ++i) {
 
 			// 1칸
 			for (int j = 1; j <= 3; ++j) {
+				if (dp[i - 1][j - 1] == -1)
+					continue;
 				dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + A[i]);
 			}
 
 			// 2칸
 			if (i >= 2) {
 				for (int j = 0; j <= 3; ++j) {
+					if (dp[i - 2][j] == -1)
+						continue;
 					dp[i][j] = Math.max(dp[i][j], dp[i - 2][j] + A[i]);
 				}
 			}
