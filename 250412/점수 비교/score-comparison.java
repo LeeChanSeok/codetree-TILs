78cbe12e28835506be4dd5f5b90e04b6403287bd
Chanseok <ckstjr93@naver.com>
2025-04-12 19:47:55 +0900

    Title: 점수 비교, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250412/점수 비교/score-comparison.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int AM = sc.nextInt();
+        int AE = sc.nextInt();
+        
+        int BM = sc.nextInt();
+        int BE = sc.nextInt();
+
+        if(AM > BM && AE > BE) System.out.println(1);
+        else System.out.println(0);
+        
+    }
+}
